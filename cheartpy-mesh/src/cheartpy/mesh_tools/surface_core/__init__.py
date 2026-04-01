1408267d8a08d347ba5f0b7b33d154e576193f07
Will Zhang <willwz@gmail.com>
2026-04-01 19:20:33 -0400

    BUG:mesh: add missing api for normalize_by_row

--- cheartpy-mesh/src/cheartpy/mesh_tools/surface_core/__init__.py
+++ cheartpy-mesh/src/cheartpy/mesh_tools/surface_core/__init__.py
@@ -1,5 +1,9 @@
 from .meshing import create_mesh_from_surface
-from .normals import compute_mesh_outer_normal_at_nodes, compute_surface_normal_at_center
+from .normals import (
+    compute_mesh_outer_normal_at_nodes,
+    compute_surface_normal_at_center,
+    normalize_by_row,
+)
 from .surfacing import create_new_surface_in_mesh, create_new_surface_in_surf
 
 __all__ = [
@@ -8,4 +12,5 @@ __all__ = [
     "create_mesh_from_surface",
     "create_new_surface_in_mesh",
     "create_new_surface_in_surf",
+    "normalize_by_row",
 ]
