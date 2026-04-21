5fc1372fa080c56c762cc5dc645944b594321b07
Will Zhang <willwz@umich.edu>
2026-04-21 18:46:29 -0400

    TYPE:cl: update stub def for export_cl_mesh function

--- cheartpy-cl/src/cheartpy/cl/mesh/__init__.pyi
+++ cheartpy-cl/src/cheartpy/cl/mesh/__init__.pyi
@@ -1,5 +1,4 @@
-from pathlib import Path
-from typing import TypedDict, Unpack
+from typing import Unpack
 
 import numpy as np
 from cheartpy.cl.types import CLDef
@@ -10,9 +9,6 @@ from ._types import APIKwargs as APIKwargs
 from ._types import CLMesh as CLMesh
 from ._types import CLPartition as CLPartition
 
-class _ExportKwargs(TypedDict, total=False):
-    root: Path
-
 def create_cl_partition[F: np.floating](defn: CLDef[F]) -> CLPartition[F]: ...
 def create_centerline_topology[F: np.floating, I: np.integer](
     mesh: CheartMesh[F, I], defn: CLDef[F], **kwargs: Unpack[APIKwargs[F]]
@@ -20,6 +16,4 @@ def create_centerline_topology[F: np.floating, I: np.integer](
 def create_centerline_topology_in_surf[F: np.floating, I: np.integer](
     mesh: CheartMesh[F, I], in_surf: int, defn: CLDef[F], **kwargs: Unpack[APIKwargs[F]]
 ) -> Result[CLMesh[F, I]]: ...
-def export_cl_mesh[F: np.floating, I: np.integer](
-    mesh: CLMesh[F, I], prefix: str, **kwargs: Unpack[_ExportKwargs]
-) -> None: ...
+def export_cl_mesh[F: np.floating, I: np.integer](mesh: CLMesh[F, I], defn: CLDef[F]) -> None: ...
