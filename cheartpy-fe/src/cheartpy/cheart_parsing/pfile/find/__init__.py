99480b62161330e5c3e3c9a3039ac366565e6ec7
Will Zhang <willwz@umich.edu>
2026-06-19 13:56:53 -0400

    ENH: fe: allow chsolve to apply macros in finding output directory
    - Refactor output directory finding logic and improve macro handling

--- cheartpy-fe/src/cheartpy/cheart_parsing/pfile/find/__init__.py
+++ cheartpy-fe/src/cheartpy/cheart_parsing/pfile/find/__init__.py
@@ -1,17 +1,23 @@
-from typing import TYPE_CHECKING
+from pathlib import Path
 
-from cheartpy.cheart_parsing.pfile._regex import OutputPath, get_output_path
-
-if TYPE_CHECKING:
-    from pathlib import Path
+from cheartpy.cheart_parsing.pfile._regex import OutputPath, get_macro, get_output_path
 
 
 def find_output_dir(pfile: Path) -> Path | None:
     text = pfile.read_text()
+    macros = {
+        m.name: m.value for line in text.splitlines() if (m := get_macro(line.strip())) is not None
+    }
+    path = None
     for line in text.splitlines():
         match get_output_path(line.strip()):
-            case OutputPath(path):
-                return path
+            case OutputPath(p):
+                path = str(p)
+                break
             case None:
                 continue
-    return None
+    if not path:
+        return None
+    for k, v in macros.items():
+        path = path.replace(f"#{k}", v)
+    return Path(path) if path else None
