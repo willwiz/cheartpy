BUG: normals: fix surface normal calculation to use cross product

cheartpy-mesh/src/cheartpy/mesh_tools/surface_core/cutplanes.py
@@ -9,7 +9,7 @@ from pytools.result import Err, Ok, Result, all_ok
 from cheartpy.mesh import import_cheart_mesh
 from cheartpy.mesh_tools.tools import MergedMesh, merge_meshes, normalize_by_row
 
-from .normals import create_mesh_from_surface
+from .normals import compute_surface_normal, create_mesh_from_surface
 
 if TYPE_CHECKING:
     from collections.abc import Mapping, Sequence
@@ -42,6 +42,7 @@ def find_cutplane_master[T](*tops: EmbbededTopologyDef[T]) -> T:
 
 def compute_householder_basis[F: np.floating](normals: A2[F]) -> A2[F]:
     mean_normal = normals.mean(axis=0)
+    print(f"{mean_normal=}")
     basis = householder_orthogonal_basis(mean_normal)
     return np.full((normals.shape[0], 9), basis.flatten())
 
@@ -92,22 +93,22 @@ def make_cutplane_topology[T](
     ):
         case Ok(bnd_meshes): ...  # fmt: skip
         case Err(e): return Err(e)  # fmt: skip
-    # match all_ok(
-    #     {k: compute_surface_normal(master_mesh, pln["bnd"]) for k, pln in cutplanes.items()}
-    # ):
-    #     case Ok(bnd_normals): ...  # fmt: skip
-    #     case Err(e): return Err(e)  # fmt: skip
-    # bnd_bases = {k: compute_householder_basis(normals) for k, normals in bnd_normals.items()}
-    # match all_ok({k: compute_zrc_basis(bnd_meshes[k].space.v, bnd_normals[k]) for k in cutplanes}):
-    #     case Ok(bnd_zrc_bases): ...  # fmt: skip
-    #     case Err(e): return Err(e)  # fmt: skip
+    match all_ok(
+        {k: compute_surface_normal(master_mesh, pln["bnd"]) for k, pln in cutplanes.items()}
+    ):
+        case Ok(bnd_normals): ...  # fmt: skip
+        case Err(e): return Err(e)  # fmt: skip
+    bnd_bases = {k: compute_householder_basis(normals) for k, normals in bnd_normals.items()}
+    match all_ok({k: compute_zrc_basis(bnd_meshes[k].space.v, bnd_normals[k]) for k in cutplanes}):
+        case Ok(bnd_zrc_bases): ...  # fmt: skip
+        case Err(e): return Err(e)  # fmt: skip
     ids = {k: pln["bnd"] * np.ones((bnd_meshes[k].space.n, 1)) for k, pln in cutplanes.items()}
     return merge_meshes(
         list(bnd_meshes.values()),
         {
-            # "Normal": list(bnd_normals.values()),
-            # "Basis": list(bnd_bases.values()),
+            "Normal": list(bnd_normals.values()),
+            "Basis": list(bnd_bases.values()),
             "IDs": list(ids.values()),
-            # "ZRC": list(bnd_zrc_bases.values()),
+            "ZRC": list(bnd_zrc_bases.values()),
         },
     ).next()

cheartpy-mesh/src/cheartpy/mesh_tools/surface_core/normals.py
@@ -29,6 +29,17 @@ _REGRESS_TOL = 0.01
 _DBL_TOL = 1.0e-14
 
 
+def compute_patch_normal[F: np.floating, I: np.integer](
+    basis: A2[np.floating],
+    space: A2[F],
+    elem: A1[I],
+    _ref_space: A2[np.floating],
+) -> A1[F]:
+    v1 = np.asarray([space[elem][:, i] @ basis[0] for i in range(3)])
+    v2 = np.asarray([space[elem][:, i] @ basis[1] for i in range(3)])
+    return np.cross(v1, v2).astype(space.dtype)
+
+
 def compute_normal_patch[F: np.floating, I: np.integer](
     basis: A2[np.floating],
     space: A2[F],
@@ -39,10 +50,14 @@ def compute_normal_patch[F: np.floating, I: np.integer](
     nodes = space[elem] - ref_space
     u = np.array([[nodes[:, i] @ b for b in basis] for i in range(3)])
     f = u + np.identity(3)
+    print(basis)
+    print(nodes)
+    print(f)
     if np.linalg.det(f) < _REGRESS_TOL:
         _g_log = get_logger()
         _g_log.debug("Element node order is inverted.")
         f = u - np.identity(3)
+        print(f)
     res, *_ = lstsq(f.T, np.array([0, 0, 1], dtype=basis.dtype))
     return res.astype(space.dtype)
 
@@ -167,8 +182,6 @@ def pack_array_to_surface_topology[F: np.floating, I: np.integer](
         for i, j in zip(old, new, strict=True)
     }
     normal_array = np.zeros_like(surf_mesh.space.v)
-    print(f"{len(normal_array)=}")
-    print(f"{len(np.unique(list(node_map.keys())))=}")
     for ns in dct_values.values():
         for b, n in ns.items():
             normal_array[node_map[b]] += n
@@ -195,7 +208,7 @@ def compute_surface_normal[F: np.floating, I: np.integer](
     )
     normals = {
         k: {
-            b: compute_normal_patch(basis, mesh.space.v, patch, vtkelem.ref)
+            b: compute_patch_normal(basis, mesh.space.v, patch, vtkelem.ref)
             for b, basis in zip(patch, interp_basis_at_refnodes, strict=True)
         }
         for k, patch in bnd_patches.items()